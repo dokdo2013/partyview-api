a814deb6e5f70719805259d065da9913b3c21d1e
hyeonwoojo <h_jo@ejn.gg>
2022-10-10 15:22:44 +0900

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { StreamerModule } from './streamer/streamer.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Partyview API')
