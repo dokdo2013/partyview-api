ff695b995a8f95ea890f74ad1f37db228067f3b3
hyeonwoojo <h_jo@ejn.gg>
2022-10-10 19:21:52 +0900

    init: sentry

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { LinkModule } from './link/link.module';
 import { StreamerModule } from './streamer/streamer.module';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    sampleRate: 1.0,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Partyview API')
